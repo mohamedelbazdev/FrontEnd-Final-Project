03d45c564128ce8b190b0c7d7997a4cc3bb906e4
Mohamed Elbaz <mohamedelbazdev@gmail.com>
2022-07-27 02:15:02 +0200

    edit chat page

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import  Pusher from "pusher-js";
 
 @Component({
   selector: 'app-chat',
@@ -7,8 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+    username = 'username';
+    messages = [];
+    message = '';
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    // Enable pusher logging - don't include this in production
+    // Pusher.logToConsole = true;
+
+    // const pusher = new Pusher('10d6ae1025ceb435d335', {
+    //   cluster: 'eu'
+    // });
+
+    // const channel = pusher.subscribe('chat');
+    // channel.bind('message', data => {
+    //   this.messages.push(data);
+    // });
   }
+
+//   submit() : void {
+//     this.http.post(url:'http://localhost:4200/api/messages',body: {
+//         username:this.username,
+//         message:this.message
+//     }).subscribe(next:() => this.message = "");
+//   }
 }
