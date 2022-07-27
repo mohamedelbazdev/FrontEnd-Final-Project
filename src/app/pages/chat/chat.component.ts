b0eb7280b18e45492faafe5e2a3321ed4d319e69
Ismail <ismailmagdy8@gmail.com>
2022-07-27 03:00:22 +0200

    Register
    Register

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import  Pusher from "pusher-js";
+// import  Pusher from "pusher-js";
 
 @Component({
   selector: 'app-chat',
