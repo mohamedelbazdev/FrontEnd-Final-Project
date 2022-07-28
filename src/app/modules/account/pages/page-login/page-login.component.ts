8bda992decd760738fd47b38e67a6a66ace5f80c
ahmednour2030 <a_mohamednour@yahoo.com>
2022-07-28 19:00:37 +0200

    update

--- src/app/modules/account/pages/page-login/page-login.component.ts
+++ src/app/modules/account/pages/page-login/page-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
     styleUrls: ['./page-login.component.scss']
 })
 export class PageLoginComponent {
-    constructor() { }
+    loginForm: FormGroup;
+    registerForm: FormGroup;
+    constructor() {
+        this.loginForm = new FormGroup({
+            email: new FormControl('', [Validators.required, Validators.email]),
+            password: new FormControl('', [Validators.required])
+        });
+        this.registerForm = new FormGroup({
+            name: new FormControl('', [Validators.required, Validators.email]),
+            email: new FormControl('', [Validators.required, Validators.email]),
+            password: new FormControl('', [Validators.required]),
+            confirmedPassword: new FormControl('', [Validators.required]),
+        });
+    }
+
+    onSubmit() {
+        if(this.loginForm.valid) {
+            console.log(this._v());
+        }
+    }
+
+    register(){
+        // if(this.registerForm.valid) {
+            console.log(this.registerForm.value);
+        // }
+    }
+    _v() {
+        return this.loginForm.value;
+    }
 }
