26d3649e2073efbdd342545957de092c16fe7198
aya3omira <104975497+aya3omira@users.noreply.github.com>
2022-07-29 11:40:52 +0200

    add to auth-services

--- src/app/modules/account/auth.service.ts
+++ src/app/modules/account/auth.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from 'express';
 
 @Injectable({
   providedIn: 'root'
 })
+
+@Injectable()
 export class AuthService {
 
-  constructor() { }
+  private _registerUrl = "http://127.0.0.1:8000/api/providers/register";
+  private _loginUrl = "http://127.0.0.1:8000/api/auth/login";
+
+  constructor(private http: HttpClient,
+              private _router: Router) { }
+
+  registerUser(user:any) {
+    return this.http.post<any>(this._registerUrl, user)
+  }
+
+  loginUser(user:any) {
+    return this.http.post<any>(this._loginUrl, user)
+  }
+
+  logoutUser() {
+    localStorage.removeItem('token')
+    // this._router.navigate(['/account'])
+  }
+
+  getToken() {
+    return localStorage.getItem('token')
+  }
+
+  loggedIn() {
+    return !!localStorage.getItem('token')
+  }
 }
