f73dac492789b5d04caf3033c8cd829d9d9c12ce
aya3omira <104975497+aya3omira@users.noreply.github.com>
2022-07-29 05:53:08 +0200

    edit login&register

--- src/app/modules/account/pages/page-login/page-login.component.ts
+++ src/app/modules/account/pages/page-login/page-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
     styleUrls: ['./page-login.component.scss']
 })
 export class PageLoginComponent {
-    constructor() { }
-}
+    form: FormGroup;
+    constructor(private readonly fb: FormBuilder) {
+      this.form = this.fb.group({
+        username: ['', Validators.required],
+        password: ['', [Validators.required, this.validatorPassword]]
+      });
+    }
+
+    submitForm() {
+      console.log(this.form.getRawValue());
+    }
+
+    validatorPassword(fc: FormControl) {
+      const value = fc.value as string;
+      const isInvalid = 'password' === value.trim().toLowerCase();
+      return isInvalid ? { passwordError: 'Password is not a strong password'} : null;
+    }}
